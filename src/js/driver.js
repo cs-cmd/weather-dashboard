285343274af347270a96c2f7d4bc06b988be7480
Ray <rayoneill.97@gmail.com>
2023-11-13 15:01:55 -0500

    Added files to push

--- src/js/driver.js
+++ src/js/driver.js
@@ -1,7 +1,6 @@
 import weatherDataService from './model/WeatherDataService.js';
 import Query from './model/Query.js';
 
-const firstQuery = new Query('Denver');
 console.log(firstQuery.formatQuery());
 
 weatherDataService.getCurrentJSON(firstQuery.formatQuery());
